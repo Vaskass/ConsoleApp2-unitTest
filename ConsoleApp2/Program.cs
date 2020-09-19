ac7189c17394fb67e30da86e71e44c0636c408e2
Vaska S <vaskas2014@gmail.com>
2020-09-19 12:21:14 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("Введите 3 значения температуры в цельсиях через enter");
+            temp temp = new temp();
+            try
+            {
+                temp.t1 = double.Parse(Console.ReadLine());
+                temp.t2 = double.Parse(Console.ReadLine());
+                temp.t3 = double.Parse(Console.ReadLine());
+
+                if (temp.isTempIce())
+                {
+                    Console.WriteLine("Среди них есть температура таяния льда");
+                }
+                else
+                {
+                    Console.WriteLine("Среди них нет температуры таяния льда");
+                }
+                goto start;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вы ввели неверное значение");
+                goto start;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp2/temp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class temp
+    {
+       public double t1 { set; get; }
+        public double t2 { set; get; }
+        public double t3 { set; get; }
+
+        public bool isTempIce()
+        {
+            if (t1 == 0 || t2 == 0 || t3 == 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
